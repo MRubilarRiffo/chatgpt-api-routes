6818ab875b3fc4de18774f69592a50e2b729c649
MRubilarRiffo <m.rubilar1010@gmail.com>
2024-10-29 15:18:44 -0300

    Cambio en nombre de varible 'name' -> 'reviewer'

--- src/services/openaiService.js
+++ src/services/openaiService.js
@@ -9,7 +9,7 @@ const generateReviewsAI = async (product, quantity = 4) => {
     const prompt = `
         Eres un generador experto de reseñas. A continuación, genera ${quantity} reseñas únicas en formato JSON. 
         Cada reseña debe tener estos campos:
-            1. **name**: El nombre del autor.
+            1. **reviewer**: El nombre del autor.
             2. **rating**: Un valor que puede ser 4 o 5.
             3. **review**: El contenido de la reseña.
 
@@ -23,8 +23,8 @@ const generateReviewsAI = async (product, quantity = 4) => {
         
         Ejemplo de JSON esperado:
         [
-            { "name": "Ricardo F.", "rating": 5, "review": "Excelente compra, llegó súper rápido." },
-            { "name": "Ana M.", "rating": 5, "review": "Desde que uso estas plantillas, mis pies están mucho mejor..." }
+            { "reviewer": "Ricardo F.", "rating": 5, "review": "Excelente compra, llegó súper rápido." },
+            { "reviewer": "Ana M.", "rating": 5, "review": "Desde que uso estas plantillas, mis pies están mucho mejor..." }
         ]
 
         Ahora, genera reseñas para el siguiente producto: "${product}"
